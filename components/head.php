3b725aca74a677d72c16319d3c8c8a64409fc3a7
bugra <@gmail.com>
2024-08-24 00:50:07 +0300

    git

---
+++ components/head.php
@@ -0,0 +1,22 @@
+<?php $head_path="/anime/" ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Anime Template">
+    <meta name="keywords" content="Anime, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title ?> | FanFables</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/plyr.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $head_path ?>css/style.css" type="text/css">
+</head>
---
+++ components/scripts.php
@@ -0,0 +1,10 @@
+<?php $scripts_path = "/anime/" ?>
+
+<script src="<?php echo $scripts_path ?>js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo $scripts_path ?>js/bootstrap.min.js"></script>
+<script src="<?php echo $scripts_path ?>js/player.js"></script>
+<script src="<?php echo $scripts_path ?>js/jquery.nice-select.min.js"></script>
+<script src="<?php echo $scripts_path ?>js/mixitup.min.js"></script>
+<script src="<?php echo $scripts_path ?>js/jquery.slicknav.js"></script>
+<script src="<?php echo $scripts_path ?>js/owl.carousel.min.js"></script>
+<script src="<?php echo $scripts_path ?>js/main.js"></script>
